f2bce89a0e5f4977e74d916cdeb6d8f3ece06154
Miras Akhmadiev <miras98.ahmadiev@gmail.com>
2026-06-01 22:35:57 +0500

    Implement shopping cart task

---
+++ src/main/java/model/Apple.java
@@ -0,0 +1,21 @@
+package model;
+
+import model.constants.Colour;
+import model.constants.Discount;
+
+public class Apple extends Food {
+    private final String colour;
+    public Apple(int amount, double price, String colour){
+        super(amount, price, true);
+        this.colour = colour;
+    }
+
+    @Override
+    public double getDiscount(){
+        if (Colour.RED.equals(colour)) {
+            return Discount.RED_APPLE_DISCOUNT_PERCENT;
+        } else {
+            return Discount.NO_DISCOUNT_PERCENT;
+        }
+    }
+}
---
+++ src/main/java/model/constants/Discount.java
@@ -0,0 +1,7 @@
+package model.constants;
+
+public class Discount {
+    public static final double RED_APPLE_DISCOUNT_PERCENT = 60;
+    public static final double NO_DISCOUNT_PERCENT = 0;
+    public static final double PERCENT_DIVISOR = 100;
+}
